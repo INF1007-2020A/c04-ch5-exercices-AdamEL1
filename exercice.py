fe9f2c2dd67c4570398607908120461fbb522db4
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2020-09-22 19:29:06 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -6,29 +6,85 @@ from typing import List
 
 
 def convert_to_absolute(number: float) -> float:
-    return 0
+    if number >= 0:
+        return number
+    else:
+        return number * -1
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
-
-    return [""]
+    words = []
+    for i in range(0, len(prefixes)):
+        words.append(prefixes[i] + suffixe)
+    return words
 
 
 def prime_integer_summation() -> int:
-    return 0
+    sum = 2
+    number = 3
+    count = 1
+    is_prime = True
+    while count < 100:
+        for i in range(3, number, 2):
+            if number % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            sum += number
+            count += 1
+        is_prime = True
+        number += 2
+    return sum
 
 
 def factorial(number: int) -> int:
-    return 0
+    product = 1
+    for i in range(1, number+1):
+        product *= i
+    return product
 
 
 def use_continue() -> None:
+    for i in range(1, 11):
+        if i == 5:
+            continue
+        print(i)
     pass
 
 
 def verify_ages(groups: List[List[int]]) -> List[bool]:
-    return []
+    i = 0
+    equipes_accepte = []
+    for group in groups:
+        if len(group) <= 3 or len(group) > 10:
+            equipes_accepte.append(False)
+            i += 1
+            continue
+        inter = i
+        for ages in group:
+            if ages == 25:
+                equipes_accepte.append(True)
+                i += 1
+                break
+        if inter != i:
+            continue
+        for ages in group:
+            if ages < 18:
+                equipes_accepte.append(False)
+                i += 1
+        if inter != i:
+            continue
+        for ages in group:
+            if ages == 50:
+                for ages2 in group:
+                    if ages2 > 70:
+                        equipes_accepte.append(False)
+                        i += 1
+        if inter != i:
+            continue
+        equipes_accepte.append(True)
+    return equipes_accepte
 
 
 def main() -> None:
